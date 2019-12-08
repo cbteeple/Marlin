76691758842590b5f0a90b90a6effb3e92d421dd
Clark Teeple <cbteeple@gmail.com>
2019-12-07 21:15:06 -0500

    Clark's custom setup

--- Marlin/src/lcd/dogm/ultralcd_DOGM.cpp
+++ Marlin/src/lcd/dogm/ultralcd_DOGM.cpp
@@ -233,7 +233,9 @@ bool MarlinUI::detected() { return true; }
     #if ENABLED(SHOW_CUSTOM_BOOTSCREEN)
       show_custom_bootscreen();
     #endif
-    show_marlin_bootscreen();
+    #if DISABLED(REPLACE_MARLIN_BOOTSCREEN)
+      show_marlin_bootscreen();
+    #endif
   }
 
 #endif // SHOW_BOOTSCREEN
--- Marlin/src/pins/ramps/pins_RAMPS.h
+++ Marlin/src/pins/ramps/pins_RAMPS.h
@@ -322,7 +322,7 @@
   //
 
   #ifndef X_SERIAL_TX_PIN
-    #define X_SERIAL_TX_PIN  40
+    #define X_SERIAL_TX_PIN  63//40
   #endif
   #ifndef X_SERIAL_RX_PIN
     #define X_SERIAL_RX_PIN  63
@@ -335,7 +335,7 @@
   #endif
 
   #ifndef Y_SERIAL_TX_PIN
-    #define Y_SERIAL_TX_PIN  59
+    #define Y_SERIAL_TX_PIN  64//59
   #endif
   #ifndef Y_SERIAL_RX_PIN
     #define Y_SERIAL_RX_PIN  64
@@ -348,7 +348,7 @@
   #endif
 
   #ifndef Z_SERIAL_TX_PIN
-    #define Z_SERIAL_TX_PIN  42
+    #define Z_SERIAL_TX_PIN  65//42
   #endif
   #ifndef Z_SERIAL_RX_PIN
     #define Z_SERIAL_RX_PIN  65
@@ -361,7 +361,7 @@
   #endif
 
   #ifndef E0_SERIAL_TX_PIN
-    #define E0_SERIAL_TX_PIN 44
+    #define E0_SERIAL_TX_PIN 66//44
   #endif
   #ifndef E0_SERIAL_RX_PIN
     #define E0_SERIAL_RX_PIN 66
