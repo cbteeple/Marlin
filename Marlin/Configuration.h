9aad27ed6be7fbba090d7d8b0373b180d89f7691
Clark Teeple <cbteeple@gmail.com>
2020-03-19 15:46:54 -0400

    update to 128 microsteps and fix manual_x_home

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1058,15 +1058,15 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 296 // Nozzle is at X4 when homed
-#define Y_BED_SIZE 298 // Nozzle is at Y2 when homed
+#define X_BED_SIZE 300 // Nozzle is at X4 when homed
+#define Y_BED_SIZE 300 // Nozzle is at Y2 when homed
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS 319 // 15mm Extra space on the right end
-#define Y_MAX_POS 306 // 4mm Extra space behind the bed
+#define X_MAX_POS 315 // 15mm Extra space on the right end
+#define Y_MAX_POS 304 // 4mm Extra space behind the bed
 #define Z_MAX_POS 400
 
 /**
@@ -1310,8 +1310,8 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-#define MANUAL_X_HOME_POS 4
-#define MANUAL_Y_HOME_POS 2
+#define MANUAL_X_HOME_POS -10
+#define MANUAL_Y_HOME_POS 0
 //#define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
